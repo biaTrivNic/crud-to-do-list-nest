a80143408117a37a30eec60861a064dcd7730dc0
Beatriz Trivillin <beatriztrivillin@nic.br>
2024-09-30 11:03:18 -0300

    habilitando cors

--- src/task/dtos/task.dto.ts
+++ src/task/dtos/task.dto.ts
@@ -1,10 +1,11 @@
-import { IsString, IsEnum, IsNotEmpty } from 'class-validator';
+import { IsString, IsEnum, IsNotEmpty, IsOptional} from 'class-validator';
 
 export class CreateTaskDto {
   @IsString({message: "o title deve ser uma string"})
   @IsNotEmpty({message: "o title nao pode estar vazio"})
   title: string;
 
+  @IsOptional() 
   @IsNotEmpty({message: "o status nao pode estar vazio"})
   @IsEnum(['done', 'pending'], {message: "o status deve ser ou 'done' ou 'pending'"})
   status: 'done' | 'pending';
